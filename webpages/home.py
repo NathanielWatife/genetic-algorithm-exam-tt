bb5eaf7188c2b650464b80af3c1d0e9541208f81
Oluwaseun Ogundeko <102489247+iamunik@users.noreply.github.com>
2025-06-09 23:35:09 +0100

    Update home.py

--- webpages/home.py
+++ webpages/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import open_picture
 
 st.set_page_config(page_title="Exam Timetable Generator - Home",
-                   page_icon="",
+                   page_icon="🗣",
                    layout="wide")
 
 st.markdown(f"""
